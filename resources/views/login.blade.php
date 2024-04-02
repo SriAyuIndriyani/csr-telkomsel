963b2202e2bf7bc6ca6b5bb92e8297667b60dddc
SriAyuIndriyani <sriayuindriani532@gmail.com>
2024-04-02 19:42:01 +0700

    Telkomsel CSR

---
+++ resources/views/login.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Login - Smart Agenda</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="/images/logo-gci-oke.png" rel="icon">
+    <link href="/images/logo-gci-oke.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body style="font-family: nunito">
+    {{-- @include('sweetalert::alert') --}}
+
+    <main>
+        <div class="container">
+            <section
+                class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                            <div class="d-flex flex-column align-items-center mb-2">
+                                <img src="/images/telkomsel.png" alt=""
+                                    class="w-50 justify-content-center">
+                            </div>
+                            <!-- End Logo -->
+                            <div class="card mb-3 bg-danger">
+                                <div class="card-body">
+                                    <div class="pt-4 pb-2">
+                                        <h5 class="card-title text-center pb-0 fs-4 text-light">Login</h5>
+                                        <p class="text-center small text-light">Silahkan login menggunakan akses Anda
+                                            untuk masuk
+                                            kehalaman dashboard.</p>
+                                    </div>
+                                    <form class="row g-3 needs-validation text-light" novalidate action="/"
+                                        method="POST">
+                                        @csrf
+                                        @if (session('success'))
+                                            <div id="success-alert" class="alert alert-success" role="alert">
+                                                {{ session('success') }}
+                                            </div>
+                                        @endif
+                                        @if (session('error'))
+                                            <div id="error-alert" class="alert alert-danger" role="alert">
+                                                {{ session('error') }}
+                                            </div>
+                                        @endif
+                                        <div class="col-12">
+                                            <label for="yourEmail" class="form-label">E-Mail</label>
+                                            <div class="input-group has-validation">
+                                                <span class="input-group-text" id="inputGroupPrepend">
+                                                    <i class="ri-mail-fill"></i>
+                                                </span>
+                                                <input type="email" name="email" class="form-control" id="yourEmail"
+                                                    required>
+                                                <div class="invalid-feedback text-dark">Silahkan masukkan email.</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <label for="yourPassword" class="form-label">Kata Sandi</label>
+                                            <div class="input-group has-validation">
+                                                <span class="input-group-text" id="inputGroupPrepend">
+                                                    <i class="ri-lock-2-fill    "></i>
+                                                </span>
+                                                <input type="password" name="password" class="form-control"
+                                                    id="yourPassword" required>
+                                                <div class="invalid-feedback text-dark">Silahkan masukkan password!
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <button class="btn btn-primary w-100" type="submit">Masuk</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="credits">
+                                Designed by <a href="">Telekomunikasi Seluler Indonesia</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+        </div>
+    </main><!-- End #main -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="/assets/vendor/quill/quill.min.js"></script>
+    <script src="/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="/assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="/assets/js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</body>
+
+</html>
